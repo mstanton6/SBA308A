1a92ea2815fd98126590ead0e86efac53d190d86
mstanton6 <mstanton6@yahoo.com>
2026-01-17 14:43:06 -0500

    Getting started

--- scripts/script.js
+++ scripts/script.js
@@ -8,33 +8,51 @@ function submitrequest(event) {
     */
     event.preventDefault();
 
-    getweather();
+    getData();
 }
 
 
-async function getweather() {
+async function getData() {
 
-  const API_KEY = "live_l6K0Ng9umniilHFGbpsCigtzIp34ujnsJMKveGnWY3vHe9JRad4j4zJWJrKBmPYZ";
-  const URL = "https://api.thecatapi.com/v1/breeds/?apikey=${API_KEY}";
+    // cat key & url 
+    //const API_KEY = "live_l6K0Ng9umniilHFGbpsCigtzIp34ujnsJMKveGnWY3vHe9JRad4j4zJWJrKBmPYZ";
+    // const URL = "https://api.thecatapi.com/v1/breeds/?apikey=${API_KEY}";
 
-  try {
-    // makes request to URL for data
-    let response = await fetch(URL);
+    // openweathermap.org
+    // const API_KEY = "07314651b0da5d10af46e598b6c17473";
+    // const URL = `https://api.openweathermap.org/geo/1.0/direct?q=plymouth,MI,US&limit=2&appid=${API_KEY}`
 
-    // parse the incoming data into JSON so we can use it
-    response = await response.json();
-    let breeds = response;
+    // The Dog API
+   // const URL = 'https://api.thedogapi.com/v1/images/search?limit=10'; // images &breed_ids=121
+    const URL = 'https://api.thedogapi.com/v1/breeds/search?limit=100'; // // breeds
+    
+    const API_KEY = "live_wTOy3uULk02VbHBNCc9woslHPDaPeFot92A8gm98zQcztpWbWM7Bwd1Ba9HotVVc";
 
-    console.log(response);
 
-    breeds.forEach((breed) => {
-      const option = document.createElement("option");
-      option.value = breed.id;
-      option.textContent = breed.name;
-     // breedSelect.appendChild(option);
-    });
+    try {
+        // makes request to URL for data
+        let response = await fetch(URL, {
+            headers: {
+                "x-api-key": API_KEY,
+            },
+        });
 
-  } catch (err) {
-    console.error(err);
-  }
+        //let response = await fetch(URL);
+
+        // parse the incoming data into JSON so we can use it
+        response = await response.json();
+        let breeds = response;
+
+        console.log(response);
+
+        // breeds.forEach((breed) => {
+        //   const option = document.createElement("option");
+        //   option.value = breed.id;
+        //   option.textContent = breed.name;
+        //  // breedSelect.appendChild(option);
+        // });
+
+    } catch (err) {
+        console.error(err);
+    }
 }
