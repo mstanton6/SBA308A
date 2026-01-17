8169756c6a224e4b4e25b67ef8d92649014a9041
mstanton6 <mstanton6@yahoo.com>
2026-01-17 15:22:27 -0500

    Dropdown working

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,8 @@ const submitbtn = document.querySelector('#submit');
 
 submitbtn.addEventListener('click', submitrequest);
 
+getBreeds();  // load up the breeds
+
 function submitrequest(event) {
     /**This is the event handler function for the submitbtn button.
     *  The submitbtn button is for "Submit"
@@ -10,7 +12,43 @@ function submitrequest(event) {
 
     getData();
 }
+async function getBreeds() {
 
+    // The Dog API
+    // const URL = 'https://api.thedogapi.com/v1/images/search?limit=10'; // images &breed_ids=121
+    const URL = 'https://api.thedogapi.com/v1/breeds/search?limit=100'; // // breeds
+
+    const API_KEY = "live_wTOy3uULk02VbHBNCc9woslHPDaPeFot92A8gm98zQcztpWbWM7Bwd1Ba9HotVVc";
+
+    try {
+        // makes request to URL for data
+        let response = await fetch(URL, {
+            headers: {
+                "x-api-key": API_KEY,
+            },
+        });
+
+        // parse the incoming data into JSON so we can use it
+        response = await response.json();
+        //let breeds = response;
+
+        console.log(response);
+
+        // load the breeds into a dropdown
+
+        let breeds = response;
+
+        breeds.forEach((breed) => {
+            const option = document.createElement("option");
+            option.value = breed.id;
+            option.textContent = breed.name;
+            breedSelect.appendChild(option);
+        });
+
+    } catch (err) {
+        console.error(err);
+    }
+}
 
 async function getData() {
 
@@ -23,9 +61,9 @@ async function getData() {
     // const URL = `https://api.openweathermap.org/geo/1.0/direct?q=plymouth,MI,US&limit=2&appid=${API_KEY}`
 
     // The Dog API
-   // const URL = 'https://api.thedogapi.com/v1/images/search?limit=10'; // images &breed_ids=121
-    const URL = 'https://api.thedogapi.com/v1/breeds/search?limit=100'; // // breeds
-    
+    const URL = 'https://api.thedogapi.com/v1/images/search?limit=10'; // images &breed_ids=121
+    // const URL = 'https://api.thedogapi.com/v1/breeds/search?limit=100'; // // breeds
+
     const API_KEY = "live_wTOy3uULk02VbHBNCc9woslHPDaPeFot92A8gm98zQcztpWbWM7Bwd1Ba9HotVVc";
 
 
