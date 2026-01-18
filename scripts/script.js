582e95a9da8ed599139c2cbf02b74a900e2e3676
mstanton6 <mstanton6@yahoo.com>
2026-01-18 13:59:57 -0500

    displayImages working

--- scripts/script.js
+++ scripts/script.js
@@ -56,7 +56,7 @@ async function getImages() {
     // console.log('breed: ' + breedSelect.value);
     let thebreed = breedSelect.value;
 
-    const URL = `https://api.thedogapi.com/v1/images/search?limit=10&breed_id=${thebreed}`; // images,  &breed_ids=121
+    const URL = `https://api.thedogapi.com/v1/images/search?limit=10&breed_id=${thebreed}`; // images,  &breed_id
     console.log('url: ' + URL);
     // const URL = 'https://api.thedogapi.com/v1/breeds/search?limit=100'; // // breeds
 
@@ -83,16 +83,61 @@ async function getImages() {
             console.log(img.url);
         });
 
-
-
-        // breeds.forEach((breed) => {
-        //   const option = document.createElement("option");
-        //   option.value = breed.id;
-        //   option.textContent = breed.name;
-        //  // breedSelect.appendChild(option);
-        // });
+        // let allimages;
+        //   allimages 
+        displayImages(response);
 
     } catch (err) {
         console.error(err);
     }
 }
+
+async function putData() {
+
+    //let thebreed = breedSelect.value;  
+    const URL = `https://api.thedogapi.com/v1/images/upload`; // images,  &breed_id
+    console.log('url: ' + URL);
+    // const URL = 'https://api.thedogapi.com/v1/breeds/search?limit=100'; // // breeds
+
+    const API_KEY = "live_wTOy3uULk02VbHBNCc9woslHPDaPeFot92A8gm98zQcztpWbWM7Bwd1Ba9HotVVc";
+
+
+    try {
+        // makes request to URL for data
+        let response = await fetch(URL, {
+            method: "POST",
+            headers: {
+                "x-api-key": API_KEY,
+            
+            },
+        });
+        // body: formData
+
+        // parse the incoming data into JSON so we can use it
+        response = await response.json();
+        //let breeds = response;
+
+        console.log(response);
+
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+
+function displayImages(image) {
+    
+    let imgcontainer = document.getElementById("imageContainer");
+    imgcontainer.innerHTML = ""
+
+    image.forEach((img ) => {
+        const imgageElement = document.createElement("img");
+
+        imgageElement.src = img.url
+        imgageElement.width = 250
+
+        imgcontainer.appendChild(imgageElement);
+        });
+
+}
+
